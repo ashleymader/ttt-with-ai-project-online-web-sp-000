d788b2e8c734ebeae27e8958c06a754485d0410c
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-20 19:57:38 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,53 @@
+class Board 
+  attr_accessor :cells
+  
+  @@cells = []
+  def initialize(board = nil)
+    self.cells = Array.new(9, " ")
+  end
+  
+  def reset!
+    self.cells = Array.new(9, " ")
+  end
+  
+  def display
+    separator = "-----------"
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts separator
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts separator
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
+  
+  
+  def position(user_input)
+    self.cells[user_input.to_i-1]
+  end
+
+  def full?
+    self.cells.all? {|index| index == "X" || index == "O"}
+  end
+  
+  def turn_count
+    counter = 0 
+    self.cells.each do |value|
+      if value == "X" || value == "O"
+      counter += 1 
+      end
+    end
+    return counter
+  end
+  
+  def taken?(position) 
+    self.cells[position.to_i-1] == "X" || self.cells[position.to_i-1] == "O"
+  end
+
+  def valid_move?(position)
+    !taken?(position) && position.to_i >0 && position.to_i <=9
+  end
+  
+  def update(position, player)
+      self.cells[position.to_i-1] = player.token
+  end
+ 
+end 
---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+class Player 
+  attr_accessor :token
+  
+  def initialize(token)
+    @token = token
+  end
+  
+  #def token
+   # self.token = X
+  #end
+end
